440484685254dba5c078840f6bdf948baef6ac39
sidharthbhatia-orangewood <sidharthbhatia@orangewood.co>
2017-12-22 18:32:36 +0530

    18:32 22/12/17

---
+++ GRBL_BCNC_TEST_MEGA/DUMMY.ino
@@ -0,0 +1,15 @@
+void send_GCODE(String code)
+{
+  GRBL.println(code);
+  debug.print("code=");
+  debug.println(code);
+  delay(100);
+  while (GRBL.available())
+  {
+    if (GRBL.available())
+    {
+      debug.print(char(GRBL.read()));
+    }
+  }
+}
+
